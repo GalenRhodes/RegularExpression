161bed908c75228f06596129df63c0ba98d169ad
Galen Rhodes <galenrhodes@me.com>
2021-02-11 15:48:49 -0500

    Initial Commit

---
+++ RegularExpression/RegularExpression.h
@@ -0,0 +1,18 @@
+//
+//  RegularExpression.h
+//  RegularExpression
+//
+//  Created by Galen Rhodes on 2/11/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RegularExpression.
+FOUNDATION_EXPORT double RegularExpressionVersionNumber;
+
+//! Project version string for RegularExpression.
+FOUNDATION_EXPORT const unsigned char RegularExpressionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RegularExpression/PublicHeader.h>
+
+
