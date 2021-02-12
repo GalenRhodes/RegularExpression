300ccf2635f1d917558c3bedfb44e8c65b08a01b
Galen Rhodes <galenrhodes@me.com>
2021-02-12 10:11:56 -0500

    Added Test Example.

--- Tests/RegularExpressionTests/RegularExpressionTests.swift
+++ Tests/RegularExpressionTests/RegularExpressionTests.swift
@@ -10,23 +10,21 @@ import XCTest
 
 class RegularExpressionTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+    override func setUpWithError() throws {}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
+    override func tearDownWithError() throws {}
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testRegularExpression() throws {
+        let str = "Now is the time for all good men to come to the aid of their country."
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        if let rx = RegularExpression(pattern: "\\b(\\S+)") {
+            rx.forEachMatchString(in: str) { s in
+                if let str = s[1] { print("> \"\(str)\"") }
+                return false
+            }
+        }
+        else {
+            XCTFail("Malformed Regular Expression Pattern.")
         }
     }
 
